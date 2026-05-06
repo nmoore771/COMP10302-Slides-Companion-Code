1df49d8bc7dae6f3bef63db3dcff06b5514b104f
NCC Moore <60438265+nmoore771@users.noreply.github.com>
2026-05-06 14:41:17 -0400

    Create index.mjs

---
+++ 02-Basic-Servers/hello-world/index.mjs
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log("Server: now listening on port " + port)
+});
+
+app.get('/', (req, res) => {
+    res.type("text/plain");
+    res.send("Hello, World!");
+});
