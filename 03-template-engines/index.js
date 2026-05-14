793c4c0d2452b2f65f47a14d966a1d1ce11de3b1
Nick Moore <nicholas.moore1@mohawkcollege.ca>
2026-05-14 09:34:43 -0400

    updated handlebars example

--- 03-template-engines/index.js
+++ 03-template-engines/index.js
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import { readFile } from 'fs';
 import { engine } from 'express-handlebars';
 
 const app = express();
@@ -20,3 +21,41 @@ app.get('/hello', (req, res) => {
     })
 })
 
+app.get('/planets', (req, res) => {
+    readFile('data/solar-system.json', (err, data) => {
+       if (err) {
+           res.status(500);
+           res.end("Error! Could not find internal data file!");
+       } else {
+           const ss = JSON.parse(data);
+           res.status(200);
+           res.render('planets', {planets : ss.solarSystem.planets});
+       }
+    });
+})
+
+app.get('/planet/:name', (req, res) => {
+    readFile('data/solar-system.json', (err, data) => {
+        if (err) {
+            res.status(500);
+            res.end("Error! Could not find internal data file!");
+        } else {
+            const ss = JSON.parse(data);
+            let TPL =
+                { found : false
+                , planet : { name : req.params.name }
+                };
+            for (let i = 0; i < ss.solarSystem.planets.length; i++ ) {
+                let planet = ss.solarSystem.planets[i];
+                if (planet.name == req.params.name) {
+                    TPL.planet = planet;
+                    TPL.found = true;
+                    break;
+                }
+            }
+            console.log(TPL);
+            res.status(200);
+            res.render('planet', TPL);
+        }
+    });
+})
